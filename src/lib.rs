6af0afbde88973acb845ce764224bd1556b05ab3
Simon Andrews <simon.andrews212@gmail.com>
2018-12-07 20:53:08 -0500

    Clean up code + documentation
     - Removed requirement in egcd that a < b (turns out it doesn't matter)
     - Made documentation more clear
     - Changed num-integer version to just 0.1
     - modinverse can handle negative x values from egcd

--- src/lib.rs
+++ src/lib.rs
@@ -5,40 +5,42 @@ extern crate num_integer;
 
 use num_integer::Integer;
 
-/// Implementation of the [extended Euclidean
+/// Finds the greatest common denominator of two integers *a* and *b*, and two
+/// integers *x* and *y* such that *ax* + *by* is the greatest common
+/// denominator of *a* and *b* (Bézout coefficients).
+///
+/// This function is an implementation of the [extended Euclidean
 /// algorithm](https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm).
 ///
-/// The first argument should be smaller than the second. This is checked with an `assert!()`.
 ///
 /// ```
 /// use modinverse::egcd;
 ///
-/// let smaller = 3;
-/// let larger = 26;
-/// let (g, x, y) = egcd(smaller, larger);
+/// let a = 26;
+/// let b = 3;
+/// let (g, x, y) = egcd(a, b);
 ///
 /// assert_eq!(g, 1); // Greatest common denominator
-/// assert_eq!(x, 9); // Bézout coefficient x
-/// assert_eq!(y, -1); // Bézout coefficient y
-/// assert_eq!((smaller * x) + (larger * y), g); // Make sure it all works out according to plan
+/// assert_eq!(x, -1); // Bézout coefficient x
+/// assert_eq!(y, 9); // Bézout coefficient y
+/// assert_eq!((a * x) + (b * y), g); // Make sure it all works out according to plan
 /// ```
 pub fn egcd<T: Copy + Integer>(a: T, b: T) -> (T, T, T) {
-    assert!(a < b);
     if a == T::zero() {
-        return (b, T::zero(), T::one());
+        (b, T::zero(), T::one())
     }
     else {
         let (g, x, y) = egcd(b % a, a);
-        return (g, y - (b / a) * x, x);
+        (g, y - (b / a) * x, x)
     }
 }
 
-/// Function to calculate the [modular multiplicative
-/// inverse](https://en.wikipedia.org/wiki/Modular_multiplicative_inverse) of an integer *a* modulo
-/// *m*.
+/// Calculates the [modular multiplicative
+/// inverse](https://en.wikipedia.org/wiki/Modular_multiplicative_inverse) *x*
+/// of an integer *a* such that *ax* ≡ 1 (mod *m*).
 ///
-/// A modular multiplicative inverse may not exist. If that is the case, this function will return
-/// `None`. Otherwise, the inverse will be returned wrapped up in a `Some`.
+/// Such an integer may not exist. If so, this function will return `None`.
+/// Otherwise, the inverse will be returned wrapped up in a `Some`.
 ///
 /// ```
 /// use modinverse::modinverse;
@@ -47,20 +49,20 @@ pub fn egcd<T: Copy + Integer>(a: T, b: T) -> (T, T, T) {
 /// let does_not_exist = modinverse(4, 32);
 ///
 /// match does_exist {
-///   Some(i) => assert_eq!(i, 9),
+///   Some(x) => assert_eq!(x, 9),
 ///   None => panic!("modinverse() didn't work as expected"),
 /// }
 ///
 /// match does_not_exist {
-///   Some(i) => panic!("modinverse() found an inverse when it shouldn't have"),
+///   Some(x) => panic!("modinverse() found an inverse when it shouldn't have"),
 ///   None => {},
 /// }
 pub fn modinverse<T: Copy + Integer>(a: T, m: T) -> Option<T> {
     let (g, x, _) = egcd(a, m);
     if g != T::one() {
-        return None;
+        None
     }
     else {
-        return Some(x % m);
+        Some((x % m + m) % m)
     }
 }
